dc4c452b39bef3ec6edfd7c76be8e76166fe2bfc
Logicalcrow <logicalcrow@logicalcrow.local>
2022-09-01 15:02:37 -0400

    Actualizando provider

---
+++ src/MenuSubServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Logicalcrow\Menu;
+
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+
+class MenuSubServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->offerPublishing();
+        //$this->registerModelBindings();
+    }
+
+    protected function offerPublishing()
+    {
+        $this->publishes([
+            __DIR__ . '/../database/migrations/create_menus_tables.php.stub' => $this->getMigrationFileName('create_menus_tables.php'),
+        ], 'migrations');
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     *
+     * @return string
+     */
+    protected function getMigrationFileName($migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make($this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR)
+            ->flatMap(function ($path) use ($filesystem, $migrationFileName) {
+                return $filesystem->glob($path.'*_'.$migrationFileName);
+            })
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
+    }
+}
