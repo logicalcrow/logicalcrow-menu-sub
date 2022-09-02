e65bff092981f08d9efdd486a22641d192a9926f
Giovanni Cespedes <giovanni@logicalcrow.com>
2022-09-02 10:20:00 -0400

    Update service providers

--- src/MenuSubServiceProvider.php
+++ src/MenuSubServiceProvider.php
@@ -21,8 +21,8 @@ class MenuSubServiceProvider extends ServiceProvider
     {
         $this->publishes([
             __DIR__.'/../database/migrations/create_menus_tables.php' => $this->app->databasePath('migrations/2022_09_01_000001_create_menus_tables.php'),
-            __DIR__.'/../database/migrations/create_menus_tables.php' => $this->app->databasePath('migrations/2022_09_01_000002_create_menu_subs_tables.php'),
-            __DIR__.'/../database/migrations/create_menus_tables.php' => $this->app->databasePath('migrations/2022_09_01_000003_create_menu_users_tables.php'),
+            __DIR__.'/../database/migrations/create_menu_subs_tables.php' => $this->app->databasePath('migrations/2022_09_01_000002_create_menu_subs_tables.php'),
+            __DIR__.'/../database/migrations/create_menu_users_tables.php' => $this->app->databasePath('migrations/2022_09_01_000003_create_menu_users_tables.php'),
         ], 'Logicalcrow-Migrations');
     }
 }
